7ef23579863f8f3505f3c16d8dda4cf33b4c7317
Tim Wilder <tmwilder@gmail.com>
2013-12-16 21:17:47 -0800

    addthis amd added to complete addthis addition.

---
+++ land_calc_app/static/js/lib/addthis.js
@@ -0,0 +1,10 @@
+define(["//s7.addthis.com/js/300/addthis_widget.js#pubid=xa-52afd0bd3b47e3f2"],
+function(){
+    addthis.layers({
+        'theme' : 'transparent',
+        'share' : {
+            'position' : 'left',
+            'numPreferredServices' : 5
+        }   
+    });
+});
